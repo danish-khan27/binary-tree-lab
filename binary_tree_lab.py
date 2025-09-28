80b8e33e370e34efd3249163c217b6b8f60c3cf1
Danish <dkhans2001@gmail.com>
2025-09-27 19:40:11 -0500

    done

--- binary_tree_lab.py
+++ binary_tree_lab.py
@@ -1,15 +1,48 @@
 from typing import Optional
 
 class TreeNode:
-    def __init__(self, val: int):
+    def __init__(self, val: int = 0, left: Optional["TreeNode"] = None, right: Optional["TreeNode"] = None):
         self.val = val
-        self.left: Optional['TreeNode'] = None
-        self.right: Optional['TreeNode'] = None
+        self.left = left
+        self.right = right
 
-# TODO: Implement the max_depth function
+
 def max_depth(root: Optional[TreeNode]) -> int:
-    pass
+    """
+    Compute the maximum depth (height) of a binary tree.
 
-# TODO: Implement the lowest_common_ancestor function
+    Args:
+        root (TreeNode | None): The root node of the tree
+
+    Returns:
+        int: The maximum depth of the tree
+    """
+    if not root:
+        return 0
+    left_depth = max_depth(root.left)
+    right_depth = max_depth(root.right)
+    return 1 + max(left_depth, right_depth)
+
+
 def lowest_common_ancestor(root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-    pass
+    """
+    Find the lowest common ancestor (LCA) of two nodes in a Binary Search Tree.
+
+    Args:
+        root (TreeNode): Root of the BST
+        p (TreeNode): First node
+        q (TreeNode): Second node
+
+    Returns:
+        TreeNode: The LCA node
+    """
+    # Because it's a BST, we can use node values to decide where to go
+    current = root
+    while current:
+        if p.val < current.val and q.val < current.val:
+            current = current.left
+        elif p.val > current.val and q.val > current.val:
+            current = current.right
+        else:
+            # We are either split between left/right OR one matches current
+            return current
